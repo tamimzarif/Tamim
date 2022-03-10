bd2f432059802e15b68e8d5c0e0d34f52f0509d8
Tamim Zarif <83536084+tamimzarif@users.noreply.github.com>
2022-03-11 01:22:30 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+// Installing service worker
+const CACHE_NAME  = 'Tamim Zarif';
+
+/* Add relative URL of all the static content you want to store in
+ * cache storage (this will help us use our app offline)*/
+let resourcesToCache = ["./", ".icon.png", "./index.js", "./styles.css"];
+
+self.addEventListener("install", e=>{
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache =>{
+            return cache.addAll(resourcesToCache);
+        })
+    );
+});
+
+
+// Cache and return requests
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    );
+});
+
+// Update a service worker
+const cacheWhitelist = ['Tamim Zarif'];
+self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      );
+    });
